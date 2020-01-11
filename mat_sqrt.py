3303232645bf34547c58d563b391da55c0c500bf
bubble-07 <spagrab@gmail.com>
2020-01-11 17:22:17 -0500

    Added mat_sqrt

--- mat_sqrt.py
+++ mat_sqrt.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-#Computes the square root of a positive semi-definite matrix
+#Computes the square root of the "closest" positive semi-definite matrix
+#to a given hermitian matrix
 def sqrtm(X):
     D, W = np.linalg.eigh(X)
     D = np.maximum(0, D) #All must be non-negative due to the PSD assumption
