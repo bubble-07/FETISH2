d8384d07c496a8c0102636c38eb8975d8f5ab180
bubble-07 <spagrab@gmail.com>
2020-02-19 19:16:29 -0500

    Remove extraneous files

--- quadrature.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import scipy as sp
-
-#Given a collection of input points
-#[d_in x n], and a collection of output points
-#[d_out x n], and a number of interior points to sample
-#[N], compute the [interpolated] monte 
